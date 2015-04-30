115701910c878bbe7e17731edc74b99dbd30dd1d
Jan_Hybs <jan.hybs@tul.cz>
2015-04-30 13:59:25 +0200

    Extract auth module to single file

---
+++ src/auth.py
@@ -0,0 +1,75 @@
+# encoding: utf-8
+# author:   Jan Hybs
+
+import functools, uuid
+from flask import request, session
+from flask_login import LoginManager, UserMixin, login_required, login_user, flash, current_user, logout_user
+
+
+class Auth (object):
+    def __init__(self, app):
+        """Static initialization"""
+        self.app = app
+        self.login_manager = LoginManager ()
+        self.login_manager.init_app (app)
+        self.login_manager.user_loader (self.load_user)
+
+        self.users = { }
+        self.check_credentials = None
+
+
+    def login (self, username, password=None, key=None):
+        """
+        Method will generate session id for user
+        and create and return User object
+        If check_credentials is set
+        it will be called
+        """
+        session_id = str (uuid.uuid4 ())
+        user = User (session_id)
+        user.username = username
+        user.password = password
+        user.key = key
+
+        if self.check_credentials is not None and self.check_credentials (user):
+            return user
+        elif self.check_credentials is None:
+            return user
+
+        return None
+
+    def login_user (self, user):
+        """Login given user"""
+        if user is not None:
+            self.users[user.id] = user
+            return login_user (user)
+        return None
+
+    def load_user (self, user_id):
+        """Loads user by user_id or return None"""
+        if user_id in self.users:
+            return self.users[user_id]
+        return None
+
+
+    def logout_user (self):
+        """Logout current user"""
+        return logout_user ()
+
+
+class User (UserMixin):
+    def __init__ (self, session_id):
+        self.id = session_id
+
+
+def authenticated_only (f):
+    @functools.wraps (f)
+    def wrapped (*args, **kwargs):
+        if not current_user.is_authenticated ():
+            print 'User not authenticated!'
+            request.namespace.disconnect ()
+        else:
+            return f (*args, **kwargs)
+
+    return wrapped
+
--- src/main.py
+++ src/main.py
@@ -1,126 +1,108 @@
 import time, random
-import uuid
+
 from flask import Flask, render_template, request, escape, redirect, url_for
 from flask_socketio import SocketIO, send, emit
 from flask_login import LoginManager, UserMixin, login_required, login_user, flash, current_user, logout_user
-import functools
+from auth import Auth, authenticated_only
 
 app = Flask (__name__)
 app.config['SECRET_KEY'] = 'secretcacas!'
-socketio = SocketIO (app)
-login_manager = LoginManager ()
-login_manager.init_app (app)
 
-users = {}
-
-def authenticated_only(f):
-    @functools.wraps(f)
-    def wrapped(*args, **kwargs):
-        if not current_user.is_authenticated():
-            print 'User not authenticated!'
-            request.namespace.disconnect()
-        else:
-            return f(*args, **kwargs)
-    return wrapped
-
-class User(UserMixin):
-    def __init__(self, username, session_id):
-        self.username = username
-        self.id = session_id
+socket = SocketIO (app)
+auth = Auth (app)
 
 
-@login_manager.user_loader
-def load_user(userid):
-    if userid in users:
-        return users[userid]
-    return None
+@app.route ("/login/<username>")
+def login (username):
+    user = auth.login (username)
+    if user is None:
+        return "wrong credentials"
+
+    auth.login_user (user)
+
+    print 'login user ' + user.username
+    print 'login id' + user.id
+    return redirect ('/')
 
 
-
-@app.route("/login/<username>")
-def login(username):
-    session_id = str(uuid.uuid4 ())
-    user = User (username, session_id)
-    login_user (user)
-
-    users[str(session_id)] = user
-    print 'login user ' + username
-    print 'login id' + session_id
-    return redirect('/')
-
-@app.route("/logout")
-def logout():
-    logout_user ()
-
-@app.route('/')
-def main():
-    return render_template('main.html')
-
-@socketio.on('connect')
-def ws_conn():
-    socketio.emit('msg', 'co co')
+@app.route ("/logout")
+def logout ():
+    auth.logout_user ()
+    return redirect ('/')
 
 
-@socketio.on('disconnect')
-def ws_disconn():
-    socketio.emit('msg', 'disco')
+@app.route ('/')
+def main ():
+    return render_template ('main.html')
 
-@socketio.on('city')
+
+@socket.on ('connect')
+def ws_conn ():
+    socket.emit ('msg', 'co co')
+
+
+@socket.on ('disconnect')
+def ws_disconn ():
+    socket.emit ('msg', 'disco')
+
+
+@socket.on ('city')
 @authenticated_only
-def ws_city(message):
+def ws_city (message):
     # print message['city']
-    emit ('city', {'city': escape(current_user.username + ": " + message['city'])})
-    auth = current_user.is_authenticated()
-    emit ('msg', str(auth))
+    emit ('city', { 'city': escape (current_user.username + ": " + message['city']) })
+    auth = current_user.is_authenticated ()
+    emit ('msg', str (auth))
 
     # i = 1
     # while True:
-    #     i+=1
-    #     time.sleep (1)
-    #     emit('city', {'city': r}, broadcast=False)
+    # i+=1
+    # time.sleep (1)
+    # emit('city', {'city': r}, broadcast=False)
+
 
 if __name__ == '__main__':
     app.debug = True
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socket.run (app, host='0.0.0.0', port=5000)
 
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     if request.method == 'POST':
-#         return "POST req"
-#     else:
-#         return "GET req"
-#
-# @app.route('/hello')
-# def hello():
-#     return 'Hello World'
-#
-#
-# @app.route('/user/<username>')
-# def show_user_profile(username):
-#     # return render_template('main.html', name=username, p=request.form['username']) 400 bad request
-#     return render_template('main.html', name=username,)
-#
-# @app.route('/post/<int:post_id>')
-# def show_post(post_id):
-#     # show the post with the given id, the id is an integer
-#     return 'Post %d' % post_id
-#
-#
-#
-# @app.route('/projects/')
-# def projects():
-#     return 'The project page'
-#
-# @app.route('/about')
-# def about():
-#     return 'The about page'
-#
-# @sockets.route('/test')
-# def test_connect(ws):
-#     while True:
-#         message = ws.receive()
-#         ws.send(message)
-#
-# if __name__ == '__main__':
-#     app.run(debug=True)
-#     # app.run(host='0.0.0.0')
+    # @app.route('/', methods=['GET', 'POST'])
+    # def index():
+    # if request.method == 'POST':
+    # return "POST req"
+    # else:
+    #         return "GET req"
+    #
+    # @app.route('/hello')
+    # def hello():
+    #     return 'Hello World'
+    #
+    #
+    # @app.route('/user/<username>')
+    # def show_user_profile(username):
+    #     # return render_template('main.html', name=username, p=request.form['username']) 400 bad request
+    #     return render_template('main.html', name=username,)
+    #
+    # @app.route('/post/<int:post_id>')
+    # def show_post(post_id):
+    #     # show the post with the given id, the id is an integer
+    #     return 'Post %d' % post_id
+    #
+    #
+    #
+    # @app.route('/projects/')
+    # def projects():
+    #     return 'The project page'
+    #
+    # @app.route('/about')
+    # def about():
+    #     return 'The about page'
+    #
+    # @sockets.route('/test')
+    # def test_connect(ws):
+    #     while True:
+    #         message = ws.receive()
+    #         ws.send(message)
+    #
+    # if __name__ == '__main__':
+    #     app.run(debug=True)
+    #     # app.run(host='0.0.0.0')
