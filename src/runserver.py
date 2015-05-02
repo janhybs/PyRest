7aca5a9bd39c179ec5b13a277fdd88fc1745d5b5
x3mSpeedy <jan.hybs@tul.cz>
2015-05-02 18:18:46 +0200

    Use database set registration for every db set root

--- src/runserver.py
+++ src/runserver.py
@@ -1,12 +1,22 @@
 import transaction
 from pyrest import app, socket, auth, db
-from pyrest.database.conference import ConferenceManagementApplication
+from pyrest.database.sets.command import CommandManagementApplication
+from pyrest.database.sets.job import JobManagementApplication
+from pyrest.database.sets.script import ScriptManagementApplication
+from pyrest.database.sets.user import UserManagementApplication
 
+# register roots for non-existing roots
+UserManagementApplication.register (db, 'users', UserManagementApplication)
+JobManagementApplication.register (db, 'jobs', JobManagementApplication)
+ScriptManagementApplication.register (db, 'scripts', ScriptManagementApplication)
+CommandManagementApplication.register (db, 'commands', CommandManagementApplication)
 
-# create 'root' for conference objects
-if not hasattr (db, 'conference'):
-    print 'no conference root, creating'
-    db.conference = ConferenceManagementApplication ()
-    transaction.commit ()
+# confirm changes
+transaction.commit ()
 
+#
+# print db.users.search_one ({'username': 'Hans', 'password': 'foo'})
+# print db.users.search_one ({})
+
+# run server
 socket.run (app, host='0.0.0.0', port=5000)
