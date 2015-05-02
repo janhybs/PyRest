42dfceead07f9bf5717083a3bb6c31050453e6ac
x3mSpeedy <jan.hybs@tul.cz>
2015-05-02 18:17:40 +0200

    Add @templated decorator

--- src/pyrest/__init__.py
+++ src/pyrest/__init__.py
@@ -2,7 +2,7 @@
 # author:   Jan Hybs
 
 import functools, ZODB.FileStorage, ZODB.DB
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_login import current_user
 from flask_socketio import SocketIO
 from pyrest.server.auth import Auth
@@ -21,7 +21,6 @@ db_conn = database.open ()
 db = db_conn.root
 
 
-
 def authenticated_only (f):
     @functools.wraps (f)
     def wrapped (*args, **kwargs):
@@ -33,8 +32,32 @@ def authenticated_only (f):
 
     return wrapped
 
-from pyrest.views import index_view, login_view
-from pyrest.sockets import  output_socket
+
+from functools import wraps
+from flask import request
 
 
-app.register_blueprint (login_view.user, url_prefix='/user')
+def templated (template=None):
+    def decorator (f):
+        @wraps (f)
+        def decorated_function (*args, **kwargs):
+            template_name = template
+            if template_name is None:
+                template_name = request.endpoint.replace ('.', '/') + '.html'
+            ctx = f (*args, **kwargs)
+            if ctx is None:
+                ctx = { }
+            elif not isinstance (ctx, dict):
+                return ctx
+            return render_template (template_name, **ctx)
+
+        return decorated_function
+
+    return decorator
+
+
+from pyrest.views import index_view, user_view
+from pyrest.sockets import output_socket
+
+
+app.register_blueprint (user_view.user, url_prefix='/user')
