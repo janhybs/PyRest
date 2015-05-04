15c3c20baed2823ee42ab0b4b9a1efbf986cf036
x3mSpeedy <jan.hybs@tul.cz>
2015-05-04 21:04:39 +0200

    Add add date formatter

--- src/pyrest/__init__.py
+++ src/pyrest/__init__.py
@@ -5,6 +5,7 @@ import functools, ZODB.FileStorage, ZODB.DB
 from flask import Flask, request, render_template, url_for, g, redirect
 from flask_login import current_user
 from flask_socketio import SocketIO
+from datetime import datetime
 
 from pyrest.server.auth import Auth
 from pyrest.server.configuration import Configuration
@@ -83,6 +84,54 @@ def inject_preview_str ():
         return string if len (string) <= max else string[0:max] + '...'
 
     return dict (str_preview=str_preview)
+
+
+def pretty_date (time=False, now_=None):
+    """
+    Get a datetime object or a int() Epoch timestamp and return a
+    pretty string like 'an hour ago', 'Yesterday', '3 months ago',
+    'just now', etc
+    """
+    now = datetime.now () if now_ is None else now_
+    if type (time) is int:
+        diff = now - datetime.fromtimestamp (time)
+    elif isinstance (time, datetime):
+        diff = now - time
+    elif not time:
+        diff = now - now
+    second_diff = diff.seconds
+    day_diff = diff.days
+
+    if day_diff < 0:
+        return ''
+
+    if day_diff == 0:
+        if second_diff < 10:
+            return "just now"
+        if second_diff < 60:
+            return str (second_diff) + " seconds ago"
+        if second_diff < 120:
+            return "a minute ago"
+        if second_diff < 3600:
+            return str (second_diff / 60) + " minutes ago"
+        if second_diff < 7200:
+            return "an hour ago"
+        if second_diff < 86400:
+            return str (second_diff / 3600) + " hours ago"
+    if day_diff == 1:
+        return "Yesterday"
+    if day_diff < 7:
+        return str (day_diff) + " days ago"
+    if day_diff < 31:
+        return str (day_diff / 7) + " weeks ago"
+    if day_diff < 365:
+        return str (day_diff / 30) + " months ago"
+    return str (day_diff / 365) + " years ago"
+
+
+@app.context_processor
+def inject_pretty_date ():
+    return dict (pretty_date=pretty_date)
 
 
 def with_tittle (title, *args_, **kwargs_):
