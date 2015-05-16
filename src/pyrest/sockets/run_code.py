73d5fa49b0539ab7aefcfdd8a451bc6fc31c18be
x3mSpeedy <jan.hybs@tul.cz>
2015-05-16 18:22:07 +0200

    Clean up code execution, now reporting more events

--- src/pyrest/sockets/run_code.py
+++ src/pyrest/sockets/run_code.py
@@ -30,26 +30,35 @@ def socket_run_code_request (info):
 
     info = DotDict (info)
 
-    job = db.jobs.get (info.job_id)
     script = db.scripts.get (info.script_id)
     rerun = script.exit_code != ScriptExitCode.unknown
 
+    # prepare base event
     base_event = DotDict ()
-    base_event.job_id = info.job_id
     base_event.script_id = info.script_id
 
-    script.start_at = millis ()
     worst_exit_code = 0
+
+    # emit script start event
+    script_start = base_event.copy ()
+    script_start.start_at = millis ()
+    emit_event ('script-start', script_start);
+    # update script start timestamp
+    script.start_at = script_start.start_at
 
     for command in script.commands:
 
+        # prepare command event base
         command_event = base_event.copy ()
         command_event.command_id = command.id
 
-        event_start = command_event.copy ()
-        event_start.start_at = millis ()
-        emit_event ('command-start', event_start)
-        command.start_at = event_start.start_at
+        # emit command start event
+        command_start = command_event.copy ()
+        command_start.start_at = millis ()
+        emit_event ('command-start', command_start)
+        # update command attr
+        command.start_at = command_start.start_at
+
 
         process = AsyncProcess (command.get_source ())
         (o, e) = process.run ()
@@ -61,9 +70,11 @@ def socket_run_code_request (info):
                 lines.append ({ 'type': 'stdout', 'line': o.get () })
 
             if lines:
-                output_event = command_event.copy ()
-                output_event.output = lines
-                emit_event ('command-output', output_event)
+                # emit script output event
+                command_output = command_event.copy ()
+                command_output.output = lines
+                emit_event ('command-output', command_output)
+                # add lines to command list
                 command.outputLines.extend (lines)
 
             # get all stderr lines and emit them
@@ -72,25 +83,33 @@ def socket_run_code_request (info):
                 lines.append ({ 'type': 'stderr', 'line': e.get () })
 
             if lines:
-                output_event = command_event.copy ()
-                output_event.output = lines
-                emit_event ('command-output', output_event)
+                # emit script output event
+                command_output = command_event.copy ()
+                command_output.output = lines
+                emit_event ('command-output', command_output)
+                # add lines to command list
                 command.outputLines.extend (lines)
-
-            time.sleep (0.3)
 
         exit_code = process.wait ()
 
-        event_end = event_start.copy ()
-        event_end.exit_code = exit_code
-        event_end.duration = millis () - event_end.start_at
-        emit_event ('command-end', event_end)
-
-        command.duration = event_end.duration
+        # emit command end event
+        command_end = command_start.copy ()
+        command_end.exit_code = exit_code
+        command_end.duration = millis () - command_end.start_at
+        emit_event ('command-end', command_end)
+        # update command exit and duration
+        command.duration = command_end.duration
         command.exit_code = exit_code
 
         worst_exit_code = max (worst_exit_code, exit_code)
 
-    script.exit_code = worst_exit_code
-    script.duration = millis () - script.start_at
+    # emit script end event
+    script_end = script_start.copy ()
+    script_end.duration = millis () - script_end.start_at
+    script_end.exit_code = worst_exit_code
+    emit_event ('script-end', script_end)
+    # update script exit and duration
+    script.exit_code = script_end.exit_code
+    script.duration = script_end.duration
+
     transaction.commit ()
