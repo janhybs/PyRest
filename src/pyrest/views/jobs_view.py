5f798e1ef9400af63b6eab919f95f1284582419f
x3mSpeedy <jan.hybs@tul.cz>
2015-05-16 18:15:21 +0200

    Add api route for scripts

--- src/pyrest/views/jobs_view.py
+++ src/pyrest/views/jobs_view.py
@@ -3,10 +3,12 @@
 from flask.templating import render_template
 import time
 
-from pyrest import app, socket, auth, database, templated, with_tittle, authenticated_only, db, json_response, _jobs_, _api_
+from pyrest import app, socket, auth, database, templated, with_tittle, authenticated_only, db, json_response, _jobs_, \
+    _api_
 from flask import redirect, Blueprint, flash, url_for, g, request, jsonify, Response
+from pyrest.database.sets.command import CommandManagementApplication
 from pyrest.forms.create_job_form import JobForm
-
+from pyrest.database.sets.script import ScriptExitCode, ScriptManagementApplication
 
 
 @_jobs_.route ("/new", methods=['GET', 'POST'])
@@ -28,12 +30,12 @@ def list_jobs ():
 @_api_.route ('/jobs/<job_id>')
 @json_response
 def api_get_job_by_id (job_id):
-    time.sleep (.165)
+    time.sleep(0.5)
     # job_id = db.jobs.search_one ().id
     job = db.jobs.get (job_id) if job_id else None
 
     if not job:
-        return Response("no such job", status=404)
+        return Response ("no such job", status=404)
 
     return job.as_dict (peek=False)
 
@@ -41,10 +43,46 @@ def api_get_job_by_id (job_id):
 @_api_.route ('/jobs')
 @json_response
 def api_get_jobs ():
-    time.sleep (0.15)
+    time.sleep(0.5)
     jobs = db.jobs.search (sort="name")
     joblist = [job.as_dict (peek=True) for job in jobs]
     return joblist
+
+
+@_api_.route ('/scripts/<script_id>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+@_api_.route ('/scripts/', methods=['GET', 'POST', 'PUT', 'DELETE'], defaults={ 'script_id': '' })
+@json_response
+def api_scripts_by_id (script_id):
+    time.sleep(0.5)
+    script = db.scripts.get (script_id)
+
+    if request.method == 'PUT':
+        # remove insides
+        if script.exit_code == ScriptExitCode.unknown:
+            commands = request.json['commandsNew'].splitlines ()
+            script.commands = CommandManagementApplication.create_command_list (script_id=script.id, commands=commands)
+
+            return { 'update': 'ok' }
+        else:
+            cp = script.copy ()
+            commands = request.json['commandsNew'].splitlines ()
+            cp.commands = CommandManagementApplication.create_command_list (script_id=script.id, commands=commands)
+
+            return cp.as_dict ()
+
+    if request.method == "POST":
+        job_id = request.json['job_id']
+        job = db.jobs.get (job_id)
+        script = ScriptManagementApplication.create (job_id=job_id, commands=request.json['commandsNew'])
+        job.add_script (script)
+        db.scripts.add (script)
+
+        return { 'insert': 'ok' }
+
+    if request.method == 'GET':
+        return script.as_dict ()
+
+    return Response (status=404);
 
 
 @_jobs_.route ("/delete")
