d7f2efcf4bae08c7ca1d6e79aa1cc5394468d733
Jan_Hybs <jan.hybs@tul.cz>
2015-05-21 15:11:03 +0200

    Change default page after creating new job

--- src/pyrest/views/jobs_view.py
+++ src/pyrest/views/jobs_view.py
@@ -3,8 +3,7 @@
 from flask.templating import render_template
 import time
 
-from pyrest import app, socket, auth, database,  db, _jobs_, \
-    _api_
+from pyrest import app, socket, auth, database,  db, _jobs_
 from flask import redirect, Blueprint, flash, url_for, g, request, jsonify, Response
 from pyrest.database.sets.command import CommandManagementApplication
 from pyrest.forms.create_job_form import JobForm
@@ -23,7 +22,7 @@ def create_job ():
     form = JobForm ()
     if form.validate_on_submit ():
         flash (u'Job successfully created', category='success')
-        return redirect (url_for ('main'))
+        return redirect (url_for ('jobs.list_jobs'))
     return render_template ('create_job_form.html', form=form)
 
 
@@ -34,78 +33,3 @@ def list_jobs ():
     shows page with all jobs created by current user
     """
     return render_template ('list_jobs.html')
-
-
-@_api_.route ('/jobs/<job_id>')
-@json_response
-def api_get_job_by_id (job_id):
-    time.sleep(0.5)
-    # job_id = db.jobs.search_one ().id
-    job = db.jobs.get (job_id) if job_id else None
-
-    if not job:
-        return Response ("no such job", status=404)
-
-    return job.as_dict (peek=False)
-
-
-@_api_.route ('/jobs')
-@json_response
-def api_get_jobs ():
-    """
-    api for jobs
-    :return:
-    """
-    time.sleep(0.5)
-    jobs = db.jobs.search (sort="name")
-    joblist = [job.as_dict (peek=True) for job in jobs]
-    return joblist
-
-
-@_api_.route ('/scripts/<script_id>', methods=['GET', 'POST', 'PUT', 'DELETE'])
-@_api_.route ('/scripts/', methods=['GET', 'POST', 'PUT', 'DELETE'], defaults={ 'script_id': '' })
-@json_response
-def api_scripts_by_id (script_id):
-    """
-    apii for scripts (creating, updating, and listing)
-    :param script_id:
-    :return:
-    """
-    time.sleep(0.5)
-    script = db.scripts.get (script_id)
-
-    if request.method == 'PUT':
-        # remove insides
-        if script.exit_code == ScriptExitCode.unknown:
-            commands = request.json['commandsNew'].splitlines ()
-            script.commands = CommandManagementApplication.create_command_list (script_id=script.id, commands=commands)
-
-            return { 'update': 'ok' }
-        else:
-            cp = script.copy ()
-            commands = request.json['commandsNew'].splitlines ()
-            cp.commands = CommandManagementApplication.create_command_list (script_id=script.id, commands=commands)
-
-            return cp.as_dict ()
-
-    if request.method == "POST":
-        job_id = request.json['job_id']
-        job = db.jobs.get (job_id)
-        script = ScriptManagementApplication.create (job_id=job_id, commands=request.json['commandsNew'])
-        job.add_script (script)
-        db.scripts.add (script)
-
-        return { 'insert': 'ok' }
-
-
-    if request.method == "DELETE":
-        if script:
-            job = db.jobs.get(script.job_id)
-            job.delete_script (script)
-            db.scripts.delete (script)
-            return { 'delete': 'ok' }
-
-    if request.method == 'GET':
-        return script.as_dict ()
-
-    return Response (status=404);
